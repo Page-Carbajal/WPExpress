3aacf073c7130cfd4aa902d4c79764c007b66459
awesomepage <page@iaaxpage.com>
2015-09-29 18:42:20 -0500

    Added composer.json .gitignore and Query.class.php

---
+++ source/Query.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Developer: Page Carbajal (https://github.com/Page-Carbajal)
+ * Date: 9/29/15, 6:37 PM
+ * Generator: PhpStorm
+ */
+
+namespace WPExpress;
+
+
+class Query
+{
+
+    public function __construct()
+    {
+
+    }
+
+}
