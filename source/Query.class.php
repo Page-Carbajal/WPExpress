81b3bfb0a9d14c7302124d62538600b3897b5989
awesomepage <page@iaaxpage.com>
2015-09-30 17:08:56 -0500

    Defined basic methods for class Query Removed IconicaMarketing/WordPress/Extensions dependency Implemented class WPExpress/Query

--- source/Query.class.php
+++ source/Query.class.php
@@ -12,15 +12,25 @@ class Query
 {
 
     protected $parameters;
+    protected $metaConditions;
+    protected $metaRelation;
+    protected $taxonomyConditions;
+    protected $taxonomyRelation;
 
     public function __construct()
     {
-        $parameters = array( 'showposts' => 10 );
+        $this->metaConditions = array();
+        $this->taxonomyConditions = array();
+        $this->metaRelation = $this->taxonomyRelation = 'AND';
+        $this->parameters = array( 'showposts' => 10 );
     }
 
     private function addParameter( $parameter, $value, $condition = null ){
         $this->parameters[$parameter] = $value;
     }
+
+
+    // Set the limit of posts to retrieve;
 
     public function all(){
         $this->limit(-1);
@@ -29,10 +39,45 @@ class Query
 
     public function limit($total){
         if( is_int( $total ) ){
-            $this->parameters['showposts']['value'] = intval( $total );
+            $this->parameters['showposts'] = intval( $total );
         }
         return $this;
     }
+
+    // Post metadata methods
+
+    public function meta($field, $value, $operator = null)
+    {
+        $compare = '';
+        switch( $operator ){
+            case "like":
+                $compare = "like";
+                break;
+            default:
+                $compare = "=";
+                break;
+        }
+
+        $this->metaConditions[] =array(
+            'key' => $field,
+            'value' => $value,
+            'compare' => $compare,
+        );
+
+        return $this;
+    }
+
+    public function metaRelation($useAND = true)
+    {
+        if( $useAND ){
+            $this->metaRelation = 'AND';
+        } else{
+            $this->metaRelation = 'OR';
+        }
+        return $this;
+    }
+
+
     // Post Methods
 //    public function ID($ID)
 //    {
@@ -48,12 +93,18 @@ class Query
 
     // Process Values
     private function buildArguments(){
+
+        if( !empty($this->metaConditions) ){
+            $this->parameters['meta_query'] = array_merge( array( 'relation' => $this->metaRelation ), $this->metaConditions);
+        }
+
         return $this->parameters;
     }
 
     public function get()
     {
-        $query = new \WP_Query( $this->buildArguments() );
+        $args = $this->buildArguments();
+        $query = new \WP_Query( $args );
         $posts = $query->get_posts();
         wp_reset_postdata();
         return $posts;
@@ -61,10 +112,30 @@ class Query
 
     // Static methods
 
+    // Instance methods
+    // This methods allow for a simple understanding of the WP_Query Abstraction Layer
+
+    /**
+     * Returns an new instance to the class Query. Sets the postType property to 'post'. Use method get to return values.
+     *
+     * @return Query
+     */
     public static function Posts()
     {
         $query = new self();
         return $query->postType('post');
+    }
+
+    /**
+     * Returns an new instance to the class Query. Sets the postType property to 'post'. Use method get to return values.
+     *
+     * @param $postType. The post type to be queried. 'post', 'page', etc...
+     * @return Query
+     */
+    public static function Custom($postType)
+    {
+        $query = new self();
+        return $query->postType( $postType );
     }
 
 }
