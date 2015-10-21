6d7e90f331fe6a8751e0af875e0f0f8b802e6db5
awesomepage <awesome@iaaxpage.com>
2015-10-21 12:44:44 -0500

    Added empty SettingsPage class Required composer to use PHP 5.3

---
+++ source/Abstractions/SettingsPage.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Developer: Page Carbajal (https://github.com/Page-Carbajal)
+ * Date: October 21 2015, 12:32 PM
+ */
+
+namespace WPExpress\Abstractions;
+
+
+abstract class SettingsPage
+{
+
+    public function __construct()
+    {
+
+    }
+
+}
